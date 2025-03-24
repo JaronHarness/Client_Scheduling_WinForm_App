70cf7b2c2d69f17e58403648dffe9b21c51a2dff
Jaron Harness <jaron.harness01@gmail.com>
2025-03-24 02:30:29 +0000

    Created Add Customer Form UI

---
+++ C969/Forms/AddCustomer.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace C969.Forms
+{
+    partial class AddCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddCustomerTitleLabel = new System.Windows.Forms.Label();
+            this.AddCustomerNameLabel = new System.Windows.Forms.Label();
+            this.AddCustomerAddressLabel = new System.Windows.Forms.Label();
+            this.AddCustomerPhoneLabel = new System.Windows.Forms.Label();
+            this.AddCustomerCityLabel = new System.Windows.Forms.Label();
+            this.AddCustomerCountryLabel = new System.Windows.Forms.Label();
+            this.AddCustomerNameTextBox = new System.Windows.Forms.TextBox();
+            this.AddCustomerAddressTextBox = new System.Windows.Forms.TextBox();
+            this.AddCustomerPhoneTextBox = new System.Windows.Forms.TextBox();
+            this.AddCustomerCityTextBox = new System.Windows.Forms.TextBox();
+            this.AddCustomerCountryTextBox = new System.Windows.Forms.TextBox();
+            this.AddCustomerSubmitButton = new System.Windows.Forms.Button();
+            this.AddCustomerCancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddCustomerTitleLabel
+            // 
+            this.AddCustomerTitleLabel.AutoSize = true;
+            this.AddCustomerTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddCustomerTitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.AddCustomerTitleLabel.Name = "AddCustomerTitleLabel";
+            this.AddCustomerTitleLabel.Size = new System.Drawing.Size(111, 20);
+            this.AddCustomerTitleLabel.TabIndex = 0;
+            this.AddCustomerTitleLabel.Text = "Add Customer";
+            // 
+            // AddCustomerNameLabel
+            // 
+            this.AddCustomerNameLabel.AutoSize = true;
+            this.AddCustomerNameLabel.Location = new System.Drawing.Point(13, 54);
+            this.AddCustomerNameLabel.Name = "AddCustomerNameLabel";
+            this.AddCustomerNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.AddCustomerNameLabel.TabIndex = 1;
+            this.AddCustomerNameLabel.Text = "Name";
+            // 
+            // AddCustomerAddressLabel
+            // 
+            this.AddCustomerAddressLabel.AutoSize = true;
+            this.AddCustomerAddressLabel.Location = new System.Drawing.Point(13, 98);
+            this.AddCustomerAddressLabel.Name = "AddCustomerAddressLabel";
+            this.AddCustomerAddressLabel.Size = new System.Drawing.Size(45, 13);
+            this.AddCustomerAddressLabel.TabIndex = 2;
+            this.AddCustomerAddressLabel.Text = "Address";
+            // 
+            // AddCustomerPhoneLabel
+            // 
+            this.AddCustomerPhoneLabel.AutoSize = true;
+            this.AddCustomerPhoneLabel.Location = new System.Drawing.Point(13, 142);
+            this.AddCustomerPhoneLabel.Name = "AddCustomerPhoneLabel";
+            this.AddCustomerPhoneLabel.Size = new System.Drawing.Size(38, 13);
+            this.AddCustomerPhoneLabel.TabIndex = 3;
+            this.AddCustomerPhoneLabel.Text = "Phone";
+            // 
+            // AddCustomerCityLabel
+            // 
+            this.AddCustomerCityLabel.AutoSize = true;
+            this.AddCustomerCityLabel.Location = new System.Drawing.Point(13, 186);
+            this.AddCustomerCityLabel.Name = "AddCustomerCityLabel";
+            this.AddCustomerCityLabel.Size = new System.Drawing.Size(24, 13);
+            this.AddCustomerCityLabel.TabIndex = 4;
+            this.AddCustomerCityLabel.Text = "City";
+            // 
+            // AddCustomerCountryLabel
+            // 
+            this.AddCustomerCountryLabel.AutoSize = true;
+            this.AddCustomerCountryLabel.Location = new System.Drawing.Point(13, 236);
+            this.AddCustomerCountryLabel.Name = "AddCustomerCountryLabel";
+            this.AddCustomerCountryLabel.Size = new System.Drawing.Size(43, 13);
+            this.AddCustomerCountryLabel.TabIndex = 5;
+            this.AddCustomerCountryLabel.Text = "Country";
+            // 
+            // AddCustomerNameTextBox
+            // 
+            this.AddCustomerNameTextBox.Location = new System.Drawing.Point(83, 51);
+            this.AddCustomerNameTextBox.Name = "AddCustomerNameTextBox";
+            this.AddCustomerNameTextBox.Size = new System.Drawing.Size(184, 20);
+            this.AddCustomerNameTextBox.TabIndex = 6;
+            // 
+            // AddCustomerAddressTextBox
+            // 
+            this.AddCustomerAddressTextBox.Location = new System.Drawing.Point(83, 95);
+            this.AddCustomerAddressTextBox.Name = "AddCustomerAddressTextBox";
+            this.AddCustomerAddressTextBox.Size = new System.Drawing.Size(184, 20);
+            this.AddCustomerAddressTextBox.TabIndex = 7;
+            // 
+            // AddCustomerPhoneTextBox
+            // 
+            this.AddCustomerPhoneTextBox.Location = new System.Drawing.Point(83, 139);
+            this.AddCustomerPhoneTextBox.Name = "AddCustomerPhoneTextBox";
+            this.AddCustomerPhoneTextBox.Size = new System.Drawing.Size(184, 20);
+            this.AddCustomerPhoneTextBox.TabIndex = 8;
+            // 
+            // AddCustomerCityTextBox
+            // 
+            this.AddCustomerCityTextBox.Location = new System.Drawing.Point(83, 183);
+            this.AddCustomerCityTextBox.Name = "AddCustomerCityTextBox";
+            this.AddCustomerCityTextBox.Size = new System.Drawing.Size(184, 20);
+            this.AddCustomerCityTextBox.TabIndex = 9;
+            // 
+            // AddCustomerCountryTextBox
+            // 
+            this.AddCustomerCountryTextBox.Location = new System.Drawing.Point(83, 233);
+            this.AddCustomerCountryTextBox.Name = "AddCustomerCountryTextBox";
+            this.AddCustomerCountryTextBox.Size = new System.Drawing.Size(184, 20);
+            this.AddCustomerCountryTextBox.TabIndex = 10;
+            // 
+            // AddCustomerSubmitButton
+            // 
+            this.AddCustomerSubmitButton.Location = new System.Drawing.Point(48, 290);
+            this.AddCustomerSubmitButton.Name = "AddCustomerSubmitButton";
+            this.AddCustomerSubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.AddCustomerSubmitButton.TabIndex = 11;
+            this.AddCustomerSubmitButton.Text = "Submit";
+            this.AddCustomerSubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // AddCustomerCancelButton
+            // 
+            this.AddCustomerCancelButton.Location = new System.Drawing.Point(162, 290);
+            this.AddCustomerCancelButton.Name = "AddCustomerCancelButton";
+            this.AddCustomerCancelButton.Size = new System.Drawing.Size(75, 23);
+            this.AddCustomerCancelButton.TabIndex = 12;
+            this.AddCustomerCancelButton.Text = "Cancel";
+            this.AddCustomerCancelButton.UseVisualStyleBackColor = true;
+            // 
+            // AddCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddCustomerCancelButton);
+            this.Controls.Add(this.AddCustomerSubmitButton);
+            this.Controls.Add(this.AddCustomerCountryTextBox);
+            this.Controls.Add(this.AddCustomerCityTextBox);
+            this.Controls.Add(this.AddCustomerPhoneTextBox);
+            this.Controls.Add(this.AddCustomerAddressTextBox);
+            this.Controls.Add(this.AddCustomerNameTextBox);
+            this.Controls.Add(this.AddCustomerCountryLabel);
+            this.Controls.Add(this.AddCustomerCityLabel);
+            this.Controls.Add(this.AddCustomerPhoneLabel);
+            this.Controls.Add(this.AddCustomerAddressLabel);
+            this.Controls.Add(this.AddCustomerNameLabel);
+            this.Controls.Add(this.AddCustomerTitleLabel);
+            this.Name = "AddCustomer";
+            this.Text = "Customer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AddCustomerTitleLabel;
+        private System.Windows.Forms.Label AddCustomerNameLabel;
+        private System.Windows.Forms.Label AddCustomerAddressLabel;
+        private System.Windows.Forms.Label AddCustomerPhoneLabel;
+        private System.Windows.Forms.Label AddCustomerCityLabel;
+        private System.Windows.Forms.Label AddCustomerCountryLabel;
+        private System.Windows.Forms.TextBox AddCustomerNameTextBox;
+        private System.Windows.Forms.TextBox AddCustomerAddressTextBox;
+        private System.Windows.Forms.TextBox AddCustomerPhoneTextBox;
+        private System.Windows.Forms.TextBox AddCustomerCityTextBox;
+        private System.Windows.Forms.TextBox AddCustomerCountryTextBox;
+        private System.Windows.Forms.Button AddCustomerSubmitButton;
+        private System.Windows.Forms.Button AddCustomerCancelButton;
+    }
+}
